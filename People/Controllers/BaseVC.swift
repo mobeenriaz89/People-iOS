4f51ace116e88e3a42bd4b880a28f24befbcbcf4
Mubeen Riaz <mobeenriaz89@gmail.com>
2023-09-07 15:21:31 +0500

    launchscreen changes

---
+++ People/Controllers/BaseVC.swift
@@ -0,0 +1,17 @@
+//
+//  BaseVC.swift
+//  People
+//
+//  Created by Mubeen Riaz on 05/09/2023.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "AppBG.pdf")!)
+    }
+    
+}
